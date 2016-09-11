8e02173efa1d469bc907ed9f6a4260ec588af0bb
Brian Picciano <BJPicciano@gmail.com>
2016-09-11 19:26:01 -0400

    init v1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Socket.io Chat
+
+A simple console based real time chat application
+
+## Requirements
+- NodeJS / NPM
+- Socket.IO
+- ExpressJS
+
+## Installation
+- Open the command line in this directory (shift rightclick)
+- Run `npm install`
+
+## Running the server
+- Run the server with `npm start` command or by running the 'start_server.bat' file
+- Go to `http://localhost:3000/` and open your browser's developer's console
---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+io.on('connection', function (socket) {
+    socket.emit('handshake');
+    // socket.broadcast.emit('say', message);
+    
+    setEventHandlers(socket);
+});
+
+var sockets = {};
+
+function setEventHandlers(socket) {
+    sockets[socket.id] = socket;
+    
+    socket.on('disconnect', function () {
+        var message = socket.username + " has disconnected.";
+
+        console.log(message);
+        socket.broadcast.emit('say', message);
+
+        delete sockets[socket.id];
+
+        socket.disconnect();
+    });
+
+    socket.on('handshake', function (name) {
+        socket.username = name;
+        var message = socket.username + " has connected.";
+
+        console.log(message);
+        socket.broadcast.emit('say', message);
+    });
+
+    socket.on('say', function (message) {
+        var nameWMessage = socket.username + ": " + message;
+
+        console.log(nameWMessage);
+
+        socket.broadcast.emit('say', nameWMessage);
+    });
+    
+    socket.on('getOnlineUsers', function () {
+        var users = [];
+        for (var id in sockets) {
+            users.push(sockets[id].username);
+        }
+        socket.emit('getOnlineUsers', users)
+    })
+}
+
+var serverPort = process.env.PORT ||3000;
+
+http.listen(serverPort, function () {
+    console.log("Server is listening on port " + serverPort);
+});
